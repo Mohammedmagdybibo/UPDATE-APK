9b80a35fbc280374a08cbfc657e0150821e21f96
Mohammedmagdybibo <82815810+Mohammedmagdybibo@users.noreply.github.com>
2025-05-19 00:50:36 +0300

    Add files via upload

---
+++ version.php
@@ -0,0 +1,8 @@
+{
+  "version": "1.3",
+  "update_url": "https://elplayer.elbatal-app.com",
+  "force_update": "true",
+  "mouscripts_stats": "true",
+  "banner1_unitid": "ca-app-pub-2977470630449223/7403262194",
+  "banner2_unitid": "ca-app-pub-2977470630449223/7949885665"
+}
